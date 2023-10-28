cadc42c3399d7c706c33d4791e2d67c8141a1a8b
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-10-28 17:15:51 +0500

    added new code

--- moduls.py
+++ moduls.py
@@ -1,5 +1,31 @@
-import math
+# import math
 
-x = 400
-print(math.sqrt(x))
-print(pow(5, 4))
+# x = 400
+# print(math.sqrt(x))
+# print(pow(5, 4))
+
+# from math import pi
+# print(pi)
+
+# print(math.log2(8))
+# print(math.log10(100))
+
+# a = 5.21
+# print(math.ceil(a))
+# print(math.floor(a))
+
+import random as r
+
+# son = r.randint(0, 100)
+# print(son)
+
+# ismlar = ['olim', 'anvar', 'hasan', 'husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choice(ism))
+
+
+x = list(range(11))
+print(x)
+r.shuffle(x)
+print(x)
